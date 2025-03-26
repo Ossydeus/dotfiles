8d7328bc22138741f25bf07711ea0beec7d1731b
Ossydeus <JocelynGreig@pm.me>
2025-03-26 14:28:43 +0000

    Add files via upload

---
+++ sway/swaylock.sh
@@ -0,0 +1,71 @@
+#! /usr/bin/bash
+#-----------------
+# See swaylock-effects on github and swaylock(1) for more details
+# To Do: 
+#	- Customise ring colours when be arsed
+if  [[ $1 = "-now" ]]; then
+    swaylock \
+    	--ignore-empty-password \
+    	--daemonize \
+    	--image ~/.config/sway/The_Manor_by_Refiend.jpg \
+    	--scaling fill \
+    	--effect-vignette 0.4:0.3 \
+    	--font='Cascadia code' \
+    	--font-size=28 \
+    	--clock \
+    	--indicator-idle-visible \
+    	--timestr '%I:%M %p' \
+    	--datestr '%a %d %b' \
+    	--indicator-radius 160 \
+    	--indicator-thickness 18 \
+        --inside-color 190f0d95 \
+        --inside-clear-color 190f0d20 \
+        --inside-ver-color 190f0d85 \
+        --inside-wrong-color 190f0d85 \
+        --line-uses-inside \
+        --ring-color 4d632190 \
+        --ring-clear-color 4d632120 \
+        --ring-ver-color 4d632195 \
+        --ring-wrong-color 190f0d85 \
+        --separator-color 190f0d85 \
+        --text-color ca9f4e \
+        --text-clear-color ca9f4e \
+        --text-ver-color ca9f4e \
+        --text-wrong-color ca9f4e \
+        --key-hl-color 4d632190 \
+        --bs-hl-color 190f0d85
+
+else
+    swaylock \
+    	--ignore-empty-password \
+    	--daemonize \
+    	--image ~/.config/sway/The_Manor_by_Refiend.jpg \
+    	--scaling fill \
+    	--effect-vignette 0.4:0.3 \
+    	--font='Cascadia code' \
+    	--font-size=24 \
+    	--clock \
+    	--indicator-idle-visible \
+    	--timestr '%I:%M %p' \
+    	--datestr '%a %d %b' \
+        --fade-in 0.6 \
+        --grace 6 \
+    	--indicator-radius 160 \
+    	--indicator-thickness 18 \
+        --inside-color 190f0d95 \
+        --inside-clear-color 190f0d20 \
+        --inside-ver-color 190f0d85 \
+        --inside-wrong-color 190f0d85 \
+        --line-uses-inside \
+        --ring-color 4d632190 \
+        --ring-clear-color 4d632120 \
+        --ring-ver-color 4d632195 \
+        --ring-wrong-color 190f0d85 \
+        --separator-color 190f0d85 \
+        --text-color ca9f4e \
+        --text-clear-color ca9f4e \
+        --text-ver-color ca9f4e \
+        --text-wrong-color ca9f4e \
+        --key-hl-color 4d632190 \
+        --bs-hl-color 190f0d85
+fi
